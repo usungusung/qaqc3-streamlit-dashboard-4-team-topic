666dd8ede3c49b4d1a9fab6ad5df9507535f2525
usungusung <usung148@gmail.com>
2025-12-11 11:48:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-import koreanize_matplotlib
 import altair as alt
 import joblib
 import json
